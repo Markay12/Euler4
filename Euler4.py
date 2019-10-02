e6c6c4758c42247c873d956a3f8fed60c2d231e0
Markay12 <54555076+Markay12@users.noreply.github.com>
2019-10-02 09:16:58 -0700

    Final Euler4 project

---
+++ Euler4.py
@@ -0,0 +1,23 @@
+import time
+
+start_time = time.time()
+
+def largePalindrome(string):
+    decide = 1
+    i = 0
+
+    while i <= int(len(string/2)) and decide == 1:
+        if string[i]!=string[-(i+1)]:
+            decide = 0
+        i += 1
+    return decide
+
+    if __name__=='__main__':
+        aux = 0
+        for k in range(101, 1000):
+            for j in range(101, 1000):
+                if largePalindrome(str(j*k)) and j * k > aux:
+                    aux = j * k
+                print(aux)
+
+    print(time.time() - start_time, " seconds")
